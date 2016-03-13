1b0b57fb63fed5309821fafb02014fdd540cdf5b
Thomas Hanley <tjhanleyx@gmail.com>
2016-03-12 18:42:33 -0800

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angInvoiceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angInvoiceApp
+ */
+angular.module('angInvoiceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
