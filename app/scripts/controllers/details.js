dbd9894c40a2ea29c115a5883516eb0f2560e5b9
Thomas Hanley <tjhanleyx@gmail.com>
2016-03-14 10:27:06 -0700

    Add Invoice Line Items

---
+++ app/scripts/controllers/details.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.controller('DetailCtrl',["$scope", "invoices",
+  function($scope, invoices){
+
+  }
+]);
--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -9,18 +9,30 @@
  */
   app.controller('InvoiceCtrl', ["$scope", "invoices",
     function($scope, invoices) {
-      $scope.invoices = invoices;
+
+      $scope.lineItems = [{'id':'lineItem_s0'}];
+
+      $scope.addNewLineItem = function(){
+        var newLineItemNo = $scope.lineItems.length+1;
+        $scope.lineItems.push({'id':'lineItem_'+newLineItemNo});
+      };
+
+      $scope.removeLineItem = function(index){
+        $scope.lineItems.splice(index,1);
+      }
 
       $scope.addInvoice = function(){
-        console.debug($scope);
+        $scope.invoices = invoices;
         $scope.invoices.$add({
           vendor: $scope.vendor,
           date: Date.now(),
-          description: $scope.description
+          description: $scope.description,
+          lineItems: $scope.lineItems
         });
         $scope.vendor = '';
         $scope.date = '';
         $scope.description = '';
+        $scope.lineItems.length = 0;
       }
     }
 ]);
